db4a637679f0ff81c12da77bfa1f8994e285e309
FrankKai <gaokai20100801@gmail.com>
2020-11-04 19:20:56 +0800

    使用inquirer交互式生成单文件组件配置信息;

--- README.md
+++ README.md
@@ -4,6 +4,5 @@ vue2单文件组件生成器，命令行式初始化组件。
 
 ### feature
 
-1. 命令式在指定位置创建单文件组件
-2. 命令式设置name，data，computed，watch，生命周期，methods
-3. 命令式引入vuex，mapState，mapMutation
+1. 命令式设置name，data，lifecycle, computed，watch, methods
+2. 命令式引入vuex，mapState，mapMutation
--- index.js
+++ index.js
@@ -1,3 +1,156 @@
-#!/usr/bin/env node
+const inquirer = require("inquirer");
 
-console.log("dot vue");
+console.log("Hi, welcome to Dot Vue");
+
+var questions = [
+  {
+    type: "input",
+    name: "filename",
+    message: "What's your .vue filename?",
+    validate: function (value) {
+      if (value) {
+        return true;
+      }
+      return "Please enter a valid .vue filename";
+    },
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "What's your .vue name property?",
+    validate: function (value) {
+      if (value) {
+        return true;
+      }
+      return "Please enter a valid .vue name property";
+    },
+  },
+  {
+    type: "confirm",
+    name: "data",
+    message: "Do you need data property?",
+  },
+  {
+    type: "input",
+    name: "data details",
+    message: "Write your data details",
+    when(answers) {
+      return answers.data;
+    },
+    validate(value) {
+      if (value && value.split(",").length > 0) {
+        return true;
+      }
+      return 'Please enter data properties like this: foo,bar,baz';
+    },
+  },
+  {
+    type: "confirm",
+    name: "computed",
+    message: "Do you need computed property?",
+  },
+  {
+    type: "input",
+    name: "computed details",
+    message: "Write your computed details",
+    when(answers) {
+      return answers.computed;
+    },
+    validate(value) {
+      if (value && value.split(",").length > 0) {
+        return true;
+      }
+      return 'Please enter computed details like this: foo,bar,baz';
+    },
+  },
+  {
+    type: "confirm",
+    name: "watch",
+    message: "Do you need watch property?",
+  },
+  {
+    type: "input",
+    name: "watch details",
+    message: "Write your watch details",
+    when(answers) {
+      return answers.watch;
+    },
+    validate(value) {
+      if (value && value.split(",").length > 0) {
+        return true;
+      }
+      return 'Please enter watch details like this: foo,bar,baz';
+    },
+  },
+  {
+    type: "confirm",
+    name: "methods",
+    message: "Do you need methods property?",
+  },
+  {
+    type: "input",
+    name: "methods details",
+    message: "Write your methods details",
+    when(answers) {
+      return answers.methods;
+    },
+    validate(value) {
+      if (value && value.split(",").length > 0) {
+        return true;
+      }
+      return 'Please enter methods details like this: foo,bar,baz';
+    },
+  },
+  {
+    type: "checkbox",
+    message: "Select vue lifecycle hooks?",
+    name: "vue lifecycle hooks",
+    choices: [
+      {
+        name: "beforeCreate",
+      },
+      {
+        name: "created",
+      },
+      {
+        name: "beforeMount",
+      },
+      {
+        name: "mounted",
+      },
+      {
+        name: "beforeUpdate",
+      },
+      {
+        name: "updated",
+      },
+      {
+        name: "beforeDestroy",
+      },
+      {
+        name: "destroyed",
+      },
+    ],
+  },
+  {
+    type: "checkbox",
+    message: "Select vuex helpers？",
+    name: "vuex helpers",
+    choices: [
+      {
+        name: "mapState",
+      },
+      {
+        name: "mapMutations",
+      },
+      {
+        name: "mapActions",
+      },
+    ],
+  },
+];
+
+inquirer.prompt(questions).then((answers) => {
+  console.log("\nOrder receipt:");
+  console.log(JSON.stringify(answers, null, "  "));
+});
